ce7130d1f51bb20380b62876f8d09b1fee111d26
SilvaJulian <juliansilva.9@gmail.com>
2019-03-25 18:21:06 -0300

    1-AspNetCoreApp Step 1

---
+++ 1-AspNetCoreApp/answer/netCoreWorkshop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace netCoreWorkshop
+{
+    class Program
+    {
+      
+public static void Main(string[] args)
+{
+	CreateWebHostBuilder(args).Build().Run();
+}
+
+public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+	WebHost.CreateDefaultBuilder(args)
+		//.Configure(app => app.Run(context => context.Response.WriteAsync("Hello World, from ASP.NET!")));
+        .UseStartup<Startup>();
+    }
+}
